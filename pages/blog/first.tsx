4df835965539204cc7fa0050d91231ad7d62893b
Marshood <marshood.ayoub@gmail.com>
2023-04-29 17:16:47 +0300

    adding new pages with netsted routing first and second blogs

---
+++ pages/blog/first.tsx
@@ -0,0 +1,8 @@
+const first = () => {
+  return (
+    <>
+      <h1> first Blog Page</h1>
+    </>
+  )
+}
+export default first
---
+++ pages/blog/second.tsx
@@ -0,0 +1,8 @@
+const second = () => {
+  return (
+    <>
+      <h1> second Blog Page</h1>
+    </>
+  )
+}
+export default second
