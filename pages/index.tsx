69485e03e13b67f4e5958242cf4f5aa9d3d7bcf6
Marshood <marshood.ayoub@gmail.com>
2023-04-29 19:03:32 +0300

    link components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,22 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-
+import Link from 'next/link'
 const inter = Inter({ subsets: ['latin'] })
 const Home = () => {
   return (
     <>
+      <h3>
+        {' '}
+        <Link href="/product">Products</Link>
+      </h3>
+      <h3>
+        {' '}
+        <Link href="/About">About</Link>
+      </h3>
+      <h3>
+        <Link href="/blog">Blog</Link>
+      </h3>
+
+      <hr />
       <h1>Home Page</h1>
     </>
   )
--- pages/product/index.tsx
+++ pages/product/index.tsx
@@ -1,10 +1,26 @@
+import Link from 'next/link'
+
 const productArray = ['Product 1', 'Product 2', 'Product 3', 'Product 4']
-const product = () => {
+const product = ({ productId = 100 }) => {
   return (
     <>
+      <Link href="/">Home</Link>
+      <hr />
       {productArray?.map((product) => {
-        return <h2> {product}</h2>
+        return (
+          <h2>
+            <Link href={`/product/${product}`}>{product}</Link>
+          </h2>
+        )
       })}
+      <h2>
+        <Link href={`/product/${productId}`}>product {productId}</Link>
+      </h2>
+      <h2>
+        <Link href={`/product/${productId}`} replace>
+          replace statue {productId}
+        </Link>
+      </h2>
     </>
   )
 }
