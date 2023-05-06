0cfa85a3fa2e8c5346d52c9fae54ff563c42432c
Marshood <marshood.ayoub@gmail.com>
2023-05-06 23:53:54 +0300

    Fetching Paths for getStaticPaths

--- pages/posts/[postId].tsx
+++ pages/posts/[postId].tsx
@@ -25,19 +25,17 @@ const post = (props: {
 export default post
 
 export async function getStaticPaths() {
-  //ssg
+  const response = await fetch(`https://jsonplaceholder.typicode.com/posts`)
+  const data = await response?.json()
+  const paths = data.map((post: { id: any }) => {
+    return {
+      params: {
+        postId: `${post.id}`,
+      },
+    }
+  })
   return {
-    paths: [
-      {
-        params: { postId: '1' },
-      },
-      {
-        params: { postId: '2' },
-      },
-      {
-        params: { postId: '3' },
-      },
-    ],
+    paths,
     fallback: false,
   }
 }
